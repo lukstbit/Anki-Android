b7abea126ce1d8c8e03365f213a4e3083ca9e2d6
lukstbit <52494258+lukstbit@users.noreply.github.com>
2024-09-08 11:42:44 +0300

    Bump org.jlleitschuh.gradle.ktlint from 11.6.1 to 12.0.2
    Note:
    - the ktlint-disable directive was removed as it's not supported
    anymore, see relevant release https://github.com/pinterest/ktlint/releases/tag/0.50.0
    - the update brought new ktlint violations which were disabled and
    will be fixed in individual commits

--- AnkiDroid/src/main/java/com/ichi2/anki/utils/Translations.kt
+++ AnkiDroid/src/main/java/com/ichi2/anki/utils/Translations.kt
@@ -1,5 +1,3 @@
-// ktlint-disable filename
-
 /*
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
